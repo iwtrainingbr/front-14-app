d991cba11fa38e8f4cfd8bad2d11dfed83d169ac
unknown <Pedro Caio>
2022-05-07 20:05:00 -0300

    Adicionei titulo botao e input

---
+++ src/pages/RecuperarSenha/index.js
@@ -0,0 +1,24 @@
+import {Button, TextField} from "@mui/material";
+import "./style.css";
+
+
+export default function RecuperarSenha() {
+    return (
+      <div> 
+            <h1 className="titulo">Recuperar conta</h1>
+            <hr/>
+
+            <TextField fullWidth className="input" id="outlined-basic" label="Email" variant="outlined" />
+            
+            <Button fullWidth className="botao" variant="contained">Enviar</Button>        
+             
+
+            <a className="link" href="#">Cadastre-se</a>
+             
+      </div>  
+         
+    )
+
+}
+
+
