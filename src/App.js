f817a8b5ff628ff47966130fe75732d14a9c8d7b
tito.delerino <f.titodelerino@gmail.com>
2022-05-07 20:30:39 -0300

    Criar pagina de tutorial

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Example from "./pages/Example";
-import Tito from "./pages/Tito";
+import Tutorial from "./pages/Tutorial";
 
 export default function App() {
   return (
     <BrowserRouter>
-
-      Ola mundo
-
       <Routes>
         <Route path="/cadastro" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/exemplo" element={<Example/>} />
-        <Route path="/tito" element={<Tito/>} />
+        <Route path="/tutorial" element={<Tutorial/>} />
       </Routes>
-
-
     </BrowserRouter>
   );
 }
